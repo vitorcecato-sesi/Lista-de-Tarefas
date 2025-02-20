fff183d387b7c4967c30b730233f7391cf649b2a
PietroMelle <mellepietro3@gmail.com>
2025-02-20 09:23:27 -0300

    CSS melhorado do footer e do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
-import "./Header.css"
-import logoG3 from "../assets/logoG3.png"
+import "./Header.css";
+import logoG3 from "../assets/logoG3.png";
+import ImgGitHub from "../assets/github (1).png"
 
 function Header() {
-    return (
-        <header className="Header"> 
-            <h1> Exercício 8 - Lista de Tarefas com Componentes Separados </h1>
-            <img className="imagem" src={logoG3} />
-        </header>
-    )
+  return (
+    <header className="Header">
+      <a href="https://github.com/vitorcecato-sesi/Lista-de-Tarefas" target="_blank" rel="Link GitHub do Projeto">
+        <img className="imagem-git" src={ImgGitHub} />
+      </a>
+      <h1> Exercício 8 - Lista de Tarefas com Componentes Separados </h1>
+      <img className="imagem" src={logoG3} />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
