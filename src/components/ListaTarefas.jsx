6720df128e320a0e0fff9a6a67e3ac2e522fddb4
Laura Betti <bettilaura0@gamail.com>
2025-02-20 08:35:38 -0300

    function lista-tarefas

--- src/components/ListaTarefas.jsx
+++ src/components/ListaTarefas.jsx
@@ -1,3 +1,15 @@
 import {useState} from "react"
 import "./ListaTarefas.css"
 
+function ListaTarefas () {
+    const [lista,setLista] = useState()
+
+    return (
+        <>
+        
+        </>
+    )
+
+}
+
+export default ListaTarefas
